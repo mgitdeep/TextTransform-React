98f5493fce88d0100ea73ef4b2ed5f9eeee5cce6
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-03 23:42:22 +0530

    L-11

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
+import Header from './components/Header'
 import './index.css'
 
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is the latest blog post!'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ]
-
-    const loading = false
-    const showComments = true
-
-    if (loading) return <h1>Loading...</h1>
-
-    const commentBlock = <div className="comments">
-    <h3>Comments ({comments.length})</h3>
-    <ul>
-        {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-        ))}
-    </ul>
-    </div>
 
     return (
-        <div className="myclass">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
+        <>
+            <Header />
+            <div className="myclass">
+                <h1>My App</h1>
 
-            {showComments && commentBlock}
-
-        </div>
+            </div>
+        </>
     )
 }
 
