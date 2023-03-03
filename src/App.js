d5169ceda53963374e54e1b54acd3f197f686ab3
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-03 18:12:20 +0530

    L-8

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <h2>Editing App.js Component</h2>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import App from "./App";
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+    <React.StrictMode>
+        <h1>My App 2.0</h1>
+        <App />
+    </React.StrictMode>
+    </>
+  
+);
