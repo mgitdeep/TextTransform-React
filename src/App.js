75ab3af332bbd8ba84d0ea4c804dee8b76afe4f7
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-04 11:59:28 +0530

    L-13

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import './index.css'
+import FeedbackItem from './components/FeedbackItem'
 
 function App() {
 
     return (
         <>
             <Header />
+            <FeedbackItem />
             <div className="myclass">
                 <h1>My App</h1>
 
