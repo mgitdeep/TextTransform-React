e80523ebd364895dfceb45c899b41c8b8283f5d1
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-14 12:47:40 +0530

    Uppercaser webapp!

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+
+  const [ text, setText ] = useState()
+
+  const handleUpClick = (e) => {
+    console.log('Button was clicked')
+    console.log(e.target)
+    // setText("You've just clicked the button!")
+    console.log(text)
+    let newText = text.toUpperCase()
+    setText(newText)
+    console.log(text)
+    console.log(newText)
+  }
+  const handleOnchange = (e) => {
+    console.log('On change')
+    console.log(e.target.value)
+    setText(e.target.value)
+  }
   return (
     <>
     <h1>{props.heading}</h1>
@@ -10,10 +28,14 @@ export default function TextForm(props) {
         </label>
         <textarea
           className="form-control"
+          placeholder="Enter something to see the magic..."
           id="myBox"
           rows="9"
+          value={text}
+          onChange={handleOnchange}
         ></textarea>
-        <button className="btn btn-primary my-3">Convert to Uppercase</button>
+
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
       </div>
     </>
   );
